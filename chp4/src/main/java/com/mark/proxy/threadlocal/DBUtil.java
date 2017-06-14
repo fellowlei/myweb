7904c8da8c0000b2ee489ad3f4c6269bfc8e9ef7
lulei11 <lulei@jd.com>
2017-06-14 18:08:42 +0800

    test

--- chp4/src/main/java/com/mark/proxy/threadlocal/DBUtil.java
+++ chp4/src/main/java/com/mark/proxy/threadlocal/DBUtil.java
@@ -1,12 +1,17 @@
 package com.mark.proxy.threadlocal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by lulei on 2017/6/14.
  */
 public class DBUtil {
+    private static final Logger logger = LoggerFactory.getLogger(DBUtil.class);
     private static final String driver = "com.mysql.jdbc.Driver";
     private static final String url = "jdbc:mysql://localhost:3306/demo";
     private static final String username = "root";
@@ -42,6 +47,59 @@ public class DBUtil {
             e.printStackTrace();
         }finally {
             connContainer.remove();
+        }
+    }
+
+    /**
+     * 开启事物
+     */
+    public static  void beginTransaction(){
+        Connection conn = getConnection();
+        if(conn != null){
+            try{
+                conn.setAutoCommit(false);
+            }catch (SQLException e){
+                logger.error("begin transaction failure", e);
+                e.printStackTrace();
+            }finally {
+                connContainer.set(conn);
+            }
+        }
+    }
+
+    /**
+     * 提交事物
+     */
+    public static  void commitTransaction(){
+        Connection conn = getConnection();
+        if(conn != null){
+            try{
+                conn.commit();
+                conn.close();
+            }catch (SQLException e){
+                logger.error("commit transaction failure",e);
+                throw new RuntimeException(e);
+            }finally {
+                connContainer.remove();
+            }
+        }
+    }
+
+    /**
+     * 回滚事物
+     */
+    public static  void rollbackTransaction(){
+        Connection conn = getConnection();
+        if(conn != null){
+            try{
+                conn.rollback();
+                conn.close();
+            }catch (SQLException e){
+                logger.error("rollback transaction failure",e);
+                throw new RuntimeException(e);
+            }finally {
+                connContainer.remove();
+            }
         }
     }
 }
